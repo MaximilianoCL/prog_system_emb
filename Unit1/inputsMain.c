bfdb2001739c4cd2732cf66ba6a917019243e8c8
root <root@DESKTOP-07A5RHK.localdomain>
2022-03-01 18:12:15 -0600

    Actividad 5

---
+++ Unit1/inputsMain.c
@@ -0,0 +1,8 @@
+#include <stdio.h>
+
+int main (int argc, char** argv){
+
+    printf("argv[0]: %s, argv1: %s, argv2: %s, argv3: %s, argv4: %s\n", argv[0], argv[1], argv[2], argv[3], argv[4]);
+
+    return 0;
+}
