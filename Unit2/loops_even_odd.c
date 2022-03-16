0c3ffd16860d49f1a641fbd03922c5b638af66c2
MaximilianoCL <2109035@upy.edu.mx>
2022-03-15 21:10:49 -0600

    loops_even_odd

--- Unit2/loops_even_odd.c
+++ Unit2/loops_even_odd.c
@@ -1,6 +1,6 @@
 #include <stdio.h>
 #include <stdlib.h>
-#include <stdbool.h>
+
 
 int main (int argc, char** argv){
 
@@ -19,6 +19,6 @@ int main (int argc, char** argv){
     printf("\n");
     
     return 0;
- 
-    
+
+
 }
