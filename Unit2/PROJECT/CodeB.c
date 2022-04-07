d1051db9580fc50a19a4d2b4e7a2e61f4a5ba3ad
MaximilianoCL <2109035@upy.edu.mx>
2022-04-07 07:55:59 -0500

    Project

---
+++ Unit2/PROJECT/CodeB.c
@@ -0,0 +1,28 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+/* 2. Design a program that receives two numbers using argv argument and print the following arithmetical operations: 
+add 
+substraction
+multiplication
+division */ 
+
+int main (int argc, char** argv){
+
+    printf("Ingresa tus numeros: %s %s \n",argv[1],argv[2]);
+
+    int number1 = atoi(argv[1]);
+    int number2 = atoi(argv[2]);
+
+    int sum = number1 + number2;
+    float min = number1 - number2;
+    int mult = number1 * number2;
+    float div = number1 / number2;
+
+    printf ("The sum of the numbers is: %d\n", sum);
+    printf ("The substraction of the numbers is: %f\n", min);
+    printf ("The multiplication of the numbers is: %d\n", mult);
+    printf ("The division of the numbers is: %f\n", div);
+
+    return 0;
+}
